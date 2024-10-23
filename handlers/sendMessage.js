15207e5efab34fe2cad542b8e145a2963b97937e
Marky <johnmarky.dev@gmail.com>
2024-10-24 00:39:43 +0800

    update sendMessage.js

--- handlers/sendMessage.js
+++ handlers/sendMessage.js
@@ -1,38 +1,42 @@
 const axios = require("axios");
 
-const MAX_RETRIES = 3;
-
-const sendMessageWithRetries = async (options, retries = MAX_RETRIES) => {
-  try {
-    const response = await axios(options);
-    console.log('Message sent successfully:', response.data);
-    return response.data; 
-  } catch (error) {
-    if (retries > 0) {
-      console.log(`Retrying... Attempts left: ${retries}`);
-      return await sendMessageWithRetries(options, retries - 1);
-    }
-    if (error.response) {
-      console.error('Error response:', error.response.data.error);
-      throw error.response.data.error; 
-    } else {
-      console.error('Error sending message:', error.message);
-      throw error; 
-    }
-  }
-};
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = async (senderId, message, pageAccessToken) => {
-  const options = {
-    method: 'POST',
-    url: 'https://graph.facebook.com/v21.0/me/messages',
-    params: { access_token: pageAccessToken },
-    data: {
-      recipient: { id: senderId },
-      message: message,
-    },
-    timeout: 5000,
-  };
+  const maxRetries = 3;
+  let attempt = 0;
 
-  return sendMessageWithRetries(options);
+  while (attempt < maxRetries) {
+    try {
+      const options = {
+        url: 'https://graph.facebook.com/v21.0/me/messages',
+        method: 'POST',
+        data: {
+          recipient: { id: senderId },
+          message: message,
+        },
+        params: { access_token: pageAccessToken },
+        timeout: 5000,
+      };
+
+      const response = await axios(options);
+      console.log('Message sent successfully:', response.data);
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        console.error('Error response:', error.response.data.error);
+      } else {
+        console.error('Error sending message:', error.message);
+      }
+
+      attempt += 1;
+      if (attempt < maxRetries) {
+        const backoffTime = Math.pow(2, attempt) * 100;
+        console.log(`Retrying in ${backoffTime} ms...`);
+        await sleep(backoffTime);
+      } else {
+        throw new Error('Max retries reached. Failed to send message.');
+      }
+    }
+  }
 };
