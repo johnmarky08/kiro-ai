40893fb27b6d876e35bd66f1cfa4430cb64b7d69
Marky <johnmarky.dev@gmail.com>
2024-10-24 00:21:43 +0800

    update sendMessage.js

--- handlers/sendMessage.js
+++ handlers/sendMessage.js
@@ -1,5 +1,27 @@
 const axios = require("axios");
 
+const MAX_RETRIES = 3;
+
+const sendMessageWithRetries = async (options, retries = MAX_RETRIES) => {
+  try {
+    const response = await axios(options);
+    console.log('Message sent successfully:', response.data);
+    return response.data; 
+  } catch (error) {
+    if (retries > 0) {
+      console.log(`Retrying... Attempts left: ${retries}`);
+      return await sendMessageWithRetries(options, retries - 1);
+    }
+    if (error.response) {
+      console.error('Error response:', error.response.data.error);
+      throw error.response.data.error; 
+    } else {
+      console.error('Error sending message:', error.message);
+      throw error; 
+    }
+  }
+};
+
 module.exports = async (senderId, message, pageAccessToken) => {
   const options = {
     method: 'POST',
@@ -9,20 +31,8 @@ module.exports = async (senderId, message, pageAccessToken) => {
       recipient: { id: senderId },
       message: message,
     },
-    timeout: 500
+    timeout: 1000,
   };
 
-  try {
-    const response = await axios(options);
-    console.log('Message sent successfully:', response.data);
-    return response.data;
-  } catch (error) {
-    if (error.response) {
-      console.error('Error response:', error.response.data.error);
-      throw error.response.data.error;
-    } else {
-      console.error('Error sending message:', error.message);
-      throw error;
-    }
-  }
+  return sendMessageWithRetries(options);
 };
