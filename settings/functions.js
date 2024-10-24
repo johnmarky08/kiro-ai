1d89c26b465ba75798984c613ad7869cfee62117
Marky <johnmarky.dev@gmail.com>
2024-10-24 15:18:01 +0800

    remove language (only eng)

--- settings/functions.js
+++ settings/functions.js
@@ -2,7 +2,7 @@ const langText = (...args) => {
   try {
     //CREDITS TO MIRAI FOR THIS
     const lang = require(`./lang.json`);
-    let result = lang[global.config.language][args[0]][args[1]];
+    let result = lang[args[0]][args[1]];
     for (var i = args.length - 1; i > 0; i--) {
       const regEx = RegExp(`%${i - 1}`, 'g');
       result = result.replace(regEx, args[i + 1]);
