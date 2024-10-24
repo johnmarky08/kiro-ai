7590a115784c2b4558296ac1bb4ec178bec64d84
Marky <johnmarky.dev@gmail.com>
2024-10-24 15:11:25 +0800

    update uid/added lang command

--- commands/uid.js
+++ commands/uid.js
@@ -1,14 +1,16 @@
-const commandName = "test";
+const commandName = "uid";
 const version = "1.0.0";
 const permission = 0;
-const description = "For testing purposes";
+const description = "Find your UID";
 const author = "John Marky Dev";
 
 const execute = async ({ args, messenger }) => {
   try {
-    if (!args) {
-      return await messenger.send(messenger.senderID);
+    if (args == "bot") {
+      return await messenger.send((await messenger.botProfile()).id);
     }
+
+    return await messenger.send(messenger.senderID);
   } catch (error) {
     console.error(`Error in executing '${commandName}' command:`, error);
     await messenger.send("An error occurred while processing your request.");
