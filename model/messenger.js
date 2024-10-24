ac7c1968eba0034f49600a31ee8a398abf3edd6c
Marky <johnmarky.dev@gmail.com>
2024-10-24 10:47:30 +0800

    update messenger.js

--- model/messenger.js
+++ model/messenger.js
@@ -1,34 +1,39 @@
- class Messenger {
-   constructor(event, pageAccessToken) {
-     this.senderID = event.sender.id;
-     this.pageAccessToken = pageAccessToken;
-     this.event = event;
-   }
+const axios = require('axios');
 
-   async sendMessage(message) {
-     try {
-       if (typeof message === "object") {
-         return await sendMessage(this.senderID, message, this.pageAccessToken);
-       } else if (typeof message === "string") {
-         return await sendMessage(this.senderID, { text: message }, this.pageAccessToken);
-       }
-     } catch (error) {
-       console.error("Failed to send message:", error);
-       throw error;
-     }
-   }
+class Messenger {
+  constructor(event, pageAccessToken) {
+    this.senderID = event.sender.id;
+    this.pageAccessToken = pageAccessToken;
+    this.event = event;
+  }
 
-   async senderProfile(userID) {
-     try {
-       const response = await axios.get(`https://graph.facebook.com/v21.0/${userID}`, {
-         params: {
-           access_token: PAGE_ACCESS_TOKEN,
-           fields: 'id,first_name,last_name,profile_pic',
-         },
-       });
-       return response.data;
-     } catch (error) {}
-   }
- }
- 
- module.exports = Messenger;
+  async sendMessage(message) {
+    try {
+      if (typeof message === "object") {
+        return await sendMessage(this.senderID, message, this.pageAccessToken);
+      } else if (typeof message === "string") {
+        return await sendMessage(this.senderID, { text: message }, this.pageAccessToken);
+      }
+    } catch (error) {
+      console.error("Failed to send message:", error);
+      throw error;
+    }
+  }
+
+  async senderProfile(userID) {
+    try {
+      const response = await axios.get(`https://graph.facebook.com/v21.0/${userID}`, {
+        params: {
+          access_token: this.pageAccessToken,
+          fields: 'id,first_name,last_name,profile_pic',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Failed to fetch sender profile:", error);
+      throw error;
+    }
+  }
+}
+
+module.exports = Messenger;
