5ca3fbe96a27a4b9ddc36da0d19f03ff2dc224f3
Marky <johnmarky.dev@gmail.com>
2024-10-29 14:54:04 +0800

    update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -8,34 +8,32 @@ const execute = async ({ args, messenger }) => {
   try {
     const owner = await messenger.userProfile("8602548189858523");
     const messageData = {
-      recipient: { id: senderID },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "media",
-            elements: [
-              {
-                media_type: "image",
-                attachment_id: null,
-                url: owner.profile_pic,
-                buttons: [
-                  {
-                    type: "web_url",
-                    url: "https://facebook.com/johnmarky.natividad",
-                    title: owner.name,
-                    webview_height_ratio: "full",
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "media",
+          elements: [
+            {
+              media_type: "image",
+              attachment_id: null,
+              url: owner.profile_pic,
+              buttons: [
+                {
+                  type: "web_url",
+                  url: "https://facebook.com/johnmarky.natividad",
+                  title: "View Facebook >",
+                  webview_height_ratio: "full",
                   },
                 ],
-                subtitle: `Owner of ${global.config.BOTNAME} AI`,
+              subtitle: `Owner of ${global.config.BOTNAME} AI\n\n» ${owner.first_name} Senpai «`,
               },
             ],
-          },
-        },
+        }
       },
     };
     return await messenger.send(messageData);
-  } catch (error) {
+  }
+  catch (error) {
     console.error(`Error in executing '${commandName}' command:`, error);
     await messenger.send("An error occurred while processing your request.");
   }
