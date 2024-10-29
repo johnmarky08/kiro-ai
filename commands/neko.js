f8de974e592733c34282805880a3ed905fdee0c8
Marky <johnmarky.dev@gmail.com>
2024-10-29 19:25:28 +0800

    command/neko

---
+++ commands/neko.js
@@ -0,0 +1,33 @@
+const commandName = "neko";
+const version = "1.0.0";
+const permission = 0;
+const description = "Random Nekooo 😻";
+const author = "John Marky Dev";
+
+const execute = async ({ args, messenger }) => {
+  try {
+    const axios = require("axios");
+    const neko = (await axios.get("https://nekos.best/api/v2/neko")).data.results[0];
+    const attachment = {
+      type: "image",
+      payload: {
+        url: neko.url,
+        is_reusable: true,
+      },
+    };
+    await messenger.send({ attachment });
+    return await messenger.send(`Artist: ${neko.artist_name}\nView Artist: ${neko.artist_href}\nView Art: ${neko.source_url}`);
+  } catch (error) {
+    console.error(`Error in executing '${commandName}' command:`, error);
+    await messenger.send("An error occurred while processing your request.");
+  }
+};
+
+module.exports = {
+  commandName,
+  version,
+  permission,
+  description,
+  author,
+  execute,
+};
