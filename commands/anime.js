1d013cc65eeee27e3673ab7c3031dddee4f25d9c
Marky <johnmarky.dev@gmail.com>
2024-10-29 18:03:53 +0800

    command/anime

---
+++ commands/anime.js
@@ -0,0 +1,81 @@
+const commandName = "anime";
+const version = "1.0.0";
+const permission = 0;
+const description = "Random Anime Pictures";
+const author = "John Marky Dev";
+
+const execute = async ({ args, messenger }) => {
+  try {
+    const axios = require("axios");
+    const BASE_URL = "https://api.waifu.pics/";
+    const sfwTags = [
+      "waifu",
+      "neko",
+      "shinobu",
+      "megumin",
+      "bully",
+      "cuddle",
+      "cry",
+      "hug",
+      "awoo",
+      "kiss",
+      "lick",
+      "pat",
+      "smug",
+      "bonk",
+      "yeet",
+      "blush",
+      "smile",
+      "wave",
+      "highfive",
+      "handhold",
+      "nom",
+      "bite",
+      "glomp",
+      "slap",
+      "kill",
+      "kick",
+      "happy",
+      "wink",
+      "poke",
+      "dance",
+      "cringe"
+    ];
+    const nswfTags = [
+      "waifu",
+      "neko",
+      "trap",
+      "blowjob"
+    ];
+    var imageUrl = "";
+    if (!args) {
+      imageUrl = (await axios.get(BASE_URL + "sfw/waifu")).data.url;
+    } else if (!args.includes("*") && swfTags.includes(args)) {
+      imageUrl = (await axios.get(BASE_URL + "sfw" + args)).data.url;
+    } else if (args.includes("*") && nswfTags.includes(args.replace("*", ""))) {
+      imageUrl = (await axios.get(BASE_URL + "nsfw" + args.replace("*", ""))).data.url;
+    } else {
+      return await messenger.send("The tags you've given is invalid!");
+    }
+    const attachment = {
+      type: "image",
+      payload: {
+        url: imageUrl,
+        is_reusable: true,
+      },
+    };
+    return await messenger.send({ attachment });
+  } catch (error) {
+    console.error(`Error in executing '${commandName}' command:`, error);
+    await messenger.send("An error occurred while processing your request.");
+  }
+};
+
+module.exports = {
+  commandName,
+  version,
+  permission,
+  description,
+  author,
+  execute,
+};
