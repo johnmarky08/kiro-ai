f1fb5c2e7ed9560a7042a8c1b3c5dbb607771240
Marky <johnmarky.dev@gmail.com>
2024-10-29 17:19:01 +0800

    update postBackHandler.js

--- handlers/postBackHandler.js
+++ handlers/postBackHandler.js
@@ -4,8 +4,17 @@ const { runCommand } = require("../settings/functions");
 module.exports = async (event, pageAccessToken) => {
   const messenger = new Messenger(event, pageAccessToken);
   const payload = event.postback.payload;
-
-  if (payload == "HELP_COMMAND") {
-    await runCommand("help", messenger);
+  const commands = {
+    "HELP_COMMAND": "help"
+  };
+  
+  try {
+  if (commands[payload]) {
+    return await runCommand(commands[payload], messenger);
+  } else {
+    return await messenger.send("Postback not found!");
+  }
+  } catch (e) {
+    return await messenger.send("Postback Error: " + e);
   }
 };
