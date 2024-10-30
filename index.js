e80f9637b56ab28a330f7edb0754bc943bb8bb61
Marky <johnmarky.dev@gmail.com>
2024-10-30 22:51:51 +0800

    update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const VERIFY_TOKEN = 'pagebot';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 app.get('/', (request, response) => {
-  response.send('Success! :p');
+  response.send('Success! :O');
 });
 
 app.get('/webhook', (request, response) => {
