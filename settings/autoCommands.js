d32c8bfdd794b8cd245df0750efcaf5e37dc0c72
Marky <johnmarky.dev@gmail.com>
2024-11-03 18:06:57 +0800

    update autoCommands.js

--- settings/autoCommands.js
+++ settings/autoCommands.js
@@ -2,19 +2,10 @@ const screenshot = async ({ userMessage, messenger }) => {
   try {
     await messenger.send({
       attachment: {
-        type: 'image',
-        payload: {
-          is_reusable: true,
-          url: `https://api.site-shot.com/?url=${userMessage}/docs&userkey=${process.env.SITE_SHOT_API_KEY}&width=1920&height=1080`
-        }
-      }
-    });
-    return await messenger.send({
-      attachment: {
         type: 'template',
         payload: {
           template_type: 'button',
-          text: '🔗 Link detected... Generated a screenshot.',
+          text: '🔗 Link detected... Generating a screenshot.',
           buttons: [
             {
               type: 'web_url',
@@ -22,6 +13,15 @@ const screenshot = async ({ userMessage, messenger }) => {
               url: userMessage
             }
           ]
+        }
+      }
+    });
+    return await messenger.send({
+      attachment: {
+        type: 'image',
+        payload: {
+          is_reusable: true,
+          url: `https://muichiro-api.vercel.app/screenshot?api_key=muichiro&link=${userMessage}`
         }
       }
     });
